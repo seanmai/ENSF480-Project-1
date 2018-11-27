0cbc1d2947ec2f8a3e3173359f1ea1cace88dff2
Sean Mai <seandmai@gmail.com>
2018-11-26 17:10:39 -0700

    Ordinary buyer view

--- OrdinaryBuyer.java
+++ OrdinaryBuyer.java
@@ -9,28 +9,43 @@
  */
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class OrdinaryBuyer implements Buyer{
     private RegisteredBuyer register(){
-        
+
+    }
+
+    public void viewDocuments(){
+        Scanner reader = new Scanner(System.in);
+		int n;
+		do{
+            for(int i = 0; i < documents.size(); i++){
+                System.out.println("Select from the following");
+        		System.out.println("[" + i + "]" + documents.get(i).getTitle());
+        	}
+    		n = reader.nextInt();
+        } while(n < 0 && n > i);
+		reader.close();
+        documents.get(n).display();
     }
 
 	@Override
 	public void addToCart(Document doc) {
-		
+
 	}
 
 	@Override
 	public void searchDocument(String name) {
-		
+
 	}
 
 	public void placeOrder(ArrayList<Document> cart) {
-		
+
 	}
 
 	@Override
 	public void makePayment() {
-		
+
 	}
 }
